8998e2acad530405b5018009519eac433d5a75b3
kchun510 <kylechun1@gmail.com>
2026-02-10 11:49:36 -0800

    Make scripts globally avail

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env zsh
+
+BIN_DIR="$HOME/bin"
+
+mkdir -p "BIN_DIR"
 
 for file in *; do
 	if [[ -f "$file" && "$file" == *.sh && "$file" != README.md && "$file" != init.sh ]]; then
 		echo "chmod +x $file"
 		chmod +x "$file"
-		cp "$file" ~/
+		cp "$file" "$BIN_DIR/"
 	fi
 done
